29750d7345924b2947f71c7df849f376f24bfb6d
tegarfebiary <57649267+tegarfebiary@users.noreply.github.com>
2021-06-20 21:40:39 +0700

    Add files via upload

---
+++ medhealth_1_2/lib/network/api/url_api.dart
@@ -0,0 +1,8 @@
+class BASEURL {
+  static String ipAddress = "192.168.1.9";
+  static String apiRegister = "http://$ipAddress/medhealth_db/register_api.php";
+  static String apiLogin = "http://$ipAddress/medhealth_db/login_api.php";
+  static String categoryWithProduct =
+      "http://$ipAddress/medhealth_db/get_product_with_category.php";
+  static String getProduct = "http://$ipAddress/medhealth_db/get_product.php";
+}
